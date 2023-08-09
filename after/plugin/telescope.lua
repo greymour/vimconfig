2c2492ae21e6971015cb9162d83d0a8ba8b5eaa5
kurtsteiglederPlushCare <kurt@plushcare.com>
2023-08-09 16:19:55 -0700

    make telescope search dotfiles

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,5 +1,6 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', function ()
+local telescope = require('telescope')
+vim.keymap.set('n', '<leader>ff', function()
   builtin.find_files {
     find_command = { 'rg', '--files', '--iglob', '!.git', '--hidden' }
   }
@@ -7,5 +8,24 @@ end)
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ")})
+  builtin.grep_string({
+    search = vim.fn.input("Grep > ")
+  })
 end)
+
+telescope.setup {
+  defaults = {
+    vimgrep_arguments = {
+      'rg',
+      '--color=never',
+      '--no-heading',
+      '--with-filename',
+      '--line-number',
+      '--column',
+      '--smart-case',
+      '--hidden',
+      '-g!package-lock.json'
+    },
+    file_ignore_patterns = { '.git', 'node_modules', 'venv' }
+  },
+}
