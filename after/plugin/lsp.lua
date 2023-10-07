f5b30613f8fa5ea6965e39b52cddc31f4f6581a9
kurtsteiglederPlushCare <kurt@plushcare.com>
2023-10-06 22:34:25 -0700

    fix enter preselecting lua snippets and making my life even worse

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -27,13 +27,14 @@ local cmp_mappings = lsp.defaults.cmp_mappings({
   ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
   ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
   ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-  ["<C-Space>"] = cmp.mapping.complete(),
 })
 
 cmp_mappings['<Tab>'] = nil
 cmp_mappings['<S-Tab>'] = nil
+cmp_mappings['<CR>'] = nil
 
 lsp.setup_nvim_cmp({
+  cmp.PreselectMode.None,
   mapping = cmp_mappings
 })
 
