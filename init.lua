79dae89f55f905f7eeefdf841c4970a663879233
Kurt Steigleder <kurtsteigleder@gmail.com>
2023-12-03 22:31:30 -0800

    merge conflict

--- init.lua
+++ init.lua
@@ -1,1 +1,2 @@
-require("steigen")
+require("greymour")
+
---
+++ lua/greymour/lazy.lua
@@ -0,0 +1,100 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+local plugins = {
+  {
+    'nvim-telescope/telescope.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' }
+  },
+  {
+    'marko-cerovac/material.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.g.material_style = "palenight"
+      vim.cmd 'colorscheme material'
+    end
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    -- load cmp on InsertEnter
+    event = "InsertEnter",
+    -- these dependencies will only be loaded when cmp loads
+    -- dependencies are always lazy-loaded unless specified otherwise
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+    },
+    config = function()
+      -- ...
+    end,
+  },
+  {
+    "folke/trouble.nvim",
+    config = function()
+      require("trouble").setup {
+        icons = false,
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      }
+    end
+  },
+  {
+    'nvim-treesitter/nvim-treesitter',
+    run = function()
+      local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+      ts_update()
+    end,
+  },
+  { "nvim-treesitter/playground" },
+  {
+    "theprimeagen/harpoon",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    branch = { "harpoon2" }
+  },
+  { "theprimeagen/refactoring.nvim" },
+  -- I never use undotree so commenting out
+  --  "mbbill/undotree"
+  { "tpope/vim-fugitive" },
+  { "nvim-treesitter/nvim-treesitter-context" },
+  {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v1.x',
+    dependencies = {
+      -- LSP Support
+      { 'neovim/nvim-lspconfig' },
+      { 'williamboman/mason.nvim' },
+      { 'williamboman/mason-lspconfig.nvim' },
+
+      -- Autocompletion
+      { 'hrsh7th/nvim-cmp' },
+      { 'hrsh7th/cmp-buffer' },
+      { 'hrsh7th/cmp-path' },
+      { 'saadparwaiz1/cmp_luasnip' },
+      { 'hrsh7th/cmp-nvim-lsp' },
+      { 'hrsh7th/cmp-nvim-lua' },
+
+      -- Snippets
+      { 'L3MON4D3/LuaSnip' },
+      { 'rafamadriz/friendly-snippets' },
+    }
+  },
+  {
+    'lewis6991/gitsigns.nvim',
+    config = function() require('gitsigns').setup() end
+  },
+  { 'windwp/nvim-ts-autotag' }
+}
+
+require("lazy").setup(plugins)
