c6ec6f555da1a6ecbe719d4a99d800e6eac24b82
Kurt Steigleder <kurt@plushcare.com>
2024-01-17 12:00:46 -0800

    match colour of current line in netrw to current line when editing file

--- lua/greymour/lazy.lua
+++ lua/greymour/lazy.lua
@@ -26,8 +26,11 @@ local plugins = {
     config = function()
       vim.g.material_style = "palenight"
       vim.cmd 'colorscheme material'
-      -- sets all line numbers to white
+      -- sets all line numbers to white with a blue-purple background, need this to make set the current line number's
+      -- colour when editing a file
       vim.cmd ':hi LineNr guibg=#7253c6 guifg=#ffffff'
+      -- this changes the line number colour in netrw
+      vim.cmd ':hi CursorLineNr guibg=#7253c6 guifg=#ffffff'
       -- overrides the LineNr setting for lines above and below the current line to an off-white
       vim.cmd ':hi LineNrAbove guibg=none guifg=#bfbfbf'
       vim.cmd ':hi LineNrBelow guibg=none guifg=#bfbfbf'
