1bf091b9848883958bc2dd037729768bc4d169ed
Kurt Steigleder <kurt@plushcare.com>
2024-03-28 20:28:36 -0700

    make commenting remap work for adding and removing comments, insert comment characters at current indentation position

--- lua/greymour/remap.lua
+++ lua/greymour/remap.lua
@@ -97,8 +97,21 @@ local comment_table = {
   go = '//',
   rust = '//',
   python = '#',
+  bash = '#',
+  sh = '#',
+  zsh = '#',
+  dockerfile = '#',
+  toml = '#',
+  mk = '#',
+  makefile = '#',
+  make = '#',
+  yaml = '#',
 }
 
+
+local function trim(s)
+  return (s:gsub("^%s*(.-)%s*$", "%1"))
+end
 -- insert the comment character for the current file type at the start of each line in the range from the start to the
 -- end of the visual selection
 vim.keymap.set({ 'v', 'n' }, '<leader>/', function()
@@ -107,44 +120,48 @@ vim.keymap.set({ 'v', 'n' }, '<leader>/', function()
     print("no log for this filetype: ", file_type)
     return
   end
-  -- vim.fn.getpos() only works for the start and end of the PREVIOUS visual selection, so doing this exits visual mode
-  vim.cmd("normal v")
-  -- see :h getpos for what the values here are
-  local start_line = vim.fn.getpos("'<")[2]
-  local end_line = vim.fn.getpos("'>")[2]
   local comment = comment_table[file_type]
   local comment_len = string.len(comment)
-  local line_str = vim.fn.getline(start_line)
-
   if comment_len < 1 then
     print("no comment character for this filetype: ", file_type)
     return
   end
+  -- vim.fn.getpos() only works for the start and end of the PREVIOUS visual selection, so doing this exits visual mode
+  vim.cmd("normal! v")
+  -- see :h getpos for what the values here are
+  local start_line = vim.fn.getpos("'<")[2]
+  local start_col = vim.fn.indent(start_line)
+  local end_line = vim.fn.getpos("'>")[2]
+  local line_str = vim.fn.getline(start_line)
   -- the start and end of the selection don't care about whether the highlighting is from top to bottom or bottom to top
   -- so we need to move the cursor to the bottom (or top) of the selection to force the direction to be consistent
   vim.api.nvim_win_set_cursor(0, { start_line, 0 })
-  -- @TODO: make these use <Up> and <Down>, probably need to not use the "normal" cmd tho
-  --   - not using "normal" throws an error, weird - maybe rework how I'm calling this?
   local step_cmd = "j"
   local end_step = "k"
-  print('steps: ', step_cmd, end_step)
-  for _ = start_line, end_line do
-    -- this means we're adding a comment, idk how this regex actually works
-    if string.sub(string.gsub(line_str, "%s+", ""), 1, comment_len) ~= comment then
-      vim.cmd("normal ^i" .. comment .. ' ')
-      vim.cmd("stopinsert")
-      vim.cmd("normal " .. step_cmd)
-    else
-      -- @TODO: I'm assume that there's a space after the comment character, should probably do some strip stuff here
-      -- @TODO: if the first line is commented but other lines are not, this will delete the first n characters of
-      -- the non-commented lines
-      -- will need to move the `line_str` assignment to inside the loop, and then do some checking to see if the line
-      -- is commented or not
-      vim.cmd("normal ^" .. comment_len + 1 .. "x")
-      vim.cmd("normal " .. step_cmd)
+  local adding_comments = string.sub(string.gsub(line_str, "%s+", ""), 1, comment_len) ~= comment
+  if adding_comments then
+    vim.cmd("normal! ^")
+    for cur_line = start_line, end_line do
+      if trim(vim.fn.getline(cur_line)) == "" then
+        vim.cmd("normal! " .. step_cmd)
+      else
+        vim.api.nvim_win_set_cursor(0, { cur_line, start_col })
+        vim.cmd("normal! i" .. comment .. ' ')
+        vim.cmd("stopinsert")
+        vim.cmd("normal! " .. step_cmd)
+      end
+    end
+  else
+    for cur_line = start_line, end_line do
+      if trim(vim.fn.getline(cur_line)):sub(1, comment_len) ~= comment then
+        vim.cmd("normal! " .. step_cmd)
+      else
+        vim.cmd("normal! ^" .. comment_len + 1 .. "x")
+        vim.cmd("normal! " .. step_cmd)
+      end
     end
   end
-  vim.cmd("normal " .. end_step)
+  vim.cmd("normal! " .. end_step)
 end, {})
 
 -- table: {
